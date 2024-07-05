5ae4d635f2079be24c54087bfeb2f38eef9a8386
Eduardo Santos <edupaz077@gmail.com>
2024-07-05 00:40:34 -0300

    update

--- Fintech/src/Context/DataContext.tsx
+++ Fintech/src/Context/DataContext.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { Dispatch } from "react";
 import useFetch from "../Hooks/useFetch";
 
 type IDataContext = {
   data: IVenda[] | null;
   loading: boolean;
   error: string | null;
+  inicio: string;
+  final: string;
+  setInicio: Dispatch<React.SetStateAction<string>>
+  setFinal:Dispatch<React.SetStateAction<string>>
 };
 
 type IVenda = {
@@ -26,12 +30,16 @@ export const useData = () => {
 };
 
 export const DataContextProvider = ({ children }: React.PropsWithChildren) => {
+  const [inicio, setInicio] = React.useState("");
+  const [final, setFinal] = React.useState("");
+console.log(inicio);
+
   const { data, loading, error } = useFetch<IVenda[]>(
-    "https://data.origamid.dev/vendas/",
+    `https://data.origamid.dev/vendas/${inicio}&final=${final}`,
   );
 
   return (
-    <DataContext.Provider value={{ data, loading, error }}>
+    <DataContext.Provider value={{ data, loading, error,inicio, setInicio,final,setFinal}}>
       {children}
     </DataContext.Provider>
   );
